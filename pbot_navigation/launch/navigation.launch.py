fb14eb81d003ff5e01853c7de24b210f80b2fcbf
zeki <nguyenhatrung411@gmail.com>
2024-01-03 20:16:28 +0000

    up

--- pbot_navigation/launch/navigation.launch.py
+++ pbot_navigation/launch/navigation.launch.py
@@ -1,1 +1,62 @@
 import os
+
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    use_sim_time = LaunchConfiguration('use_sim_time', default='false')
+    autostart = LaunchConfiguration('autostart', default='true')
+
+    nav2param_file_name = 'navigation2_params.yaml'
+
+    nav2param_dir = LaunchConfiguration(
+        'params_file',
+        default=os.path.join(
+            get_package_share_directory('pbot_navigation'),
+            'params',
+            nav2param_file_name))
+
+    nav2_launch_file_dir = os.path.join(
+        get_package_share_directory('nav2_bringup'), 'launch')
+    
+    map_dir = LaunchConfiguration(
+        'map',
+        default='/workspaces/Project_Jackal_r2guide/pbot_ws/src/pbot_core/pbot_navigation/maps/home_room.yaml')
+    
+    # os.path.join(get_package_share_directory('pbot_navigation'), 'maps', 'home_room.yaml')
+
+    rviz_config_dir = os.path.join(
+        get_package_share_directory('nav2_bringup'),
+        'rviz',
+        'nav2_default_view.rviz')
+    
+    return LaunchDescription([
+
+        # Nav2
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                [nav2_launch_file_dir, '/bringup_launch.py']),
+            launch_arguments={
+                'map': map_dir,
+                'autostart': autostart,
+                'params_file': nav2param_dir,
+                'use_sim_time': use_sim_time}.items(),
+        ),
+
+        Node(
+            package='rviz2',
+            executable='rviz2',
+            name='rviz2',
+            output='screen',
+            arguments=['-d', rviz_config_dir],
+            parameters=[{'use_sim_time': use_sim_time}],
+        )
+
+    ])
--- pbot_navigation/launch/robot_localization.launch.py
+++ pbot_navigation/launch/robot_localization.launch.py
@@ -14,11 +14,26 @@ def generate_launch_description():
     use_sim_time = LaunchConfiguration('use_sim_time', default='false')
     
     return LaunchDescription([
-
+       Node(
+            package='imu_filter_madgwick',
+            executable='imu_filter_madgwick_node',
+            name='imu_filter_node',
+            parameters=[{
+                'gain': 0.1,
+                'zeta': 0.001,
+                'publish_tf': False,
+                'use_mag': False,
+            }],
+            # remappings=[
+            #     ('/some/default/topic', topic_name),
+            # ]
+        ),
+        
         # Robot localization
         IncludeLaunchDescription(
             PythonLaunchDescriptionSource([get_package_share_directory(
-                'robot_localization'), '/launch/ekf.launch.py'])
+                'robot_localization'), '/launch/ekf.launch.py']),
+            launch_arguments={'use_sim_time': use_sim_time}.items(),
         ),
 
     ])
